0a1ba010b5a64a243f669f257ff175c9eacd99d2
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-16 12:23:26 +0800

    add tools for data MINST

--- init.m
+++ init.m
@@ -4,8 +4,9 @@ cd(thisDir);
 addpath(thisDir);
 
 include  = @(d)addpath(genpathPMTK(d));
-include(fullfile(thisDir,'data'))
-include(fullfile(thisDir, 'tools'))
+include(fullfile(thisDir,'data'));
+include(fullfile(thisDir, 'tools'));
 include(fullfile(thisDir, 'toolbox')); 
 include(fullfile(thisDir, 'matlabTools')); 
+include(fullfile(thisDir, 'bigData'));
 include(fullfile(thisDir, 'pmtksupportCopy')); % may be initially empty
