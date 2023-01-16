ce3429ffad7f1c0ab286c32a687449ee8031d218
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-16 11:51:57 +0800

    update data and tools folder

--- init.m
+++ init.m
@@ -4,6 +4,8 @@ cd(thisDir);
 addpath(thisDir);
 
 include  = @(d)addpath(genpathPMTK(d));
-include(fullfile(thisDir, 'toolbox')); % may be initially empty
-include(fullfile(thisDir, 'matlabTools')); % may be initially empty
+include(fullfile(thisDir,'data'))
+include(fullfile(thisDir, 'tools'))
+include(fullfile(thisDir, 'toolbox')); 
+include(fullfile(thisDir, 'matlabTools')); 
 include(fullfile(thisDir, 'pmtksupportCopy')); % may be initially empty
